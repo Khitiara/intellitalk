2a47e433aa5bb7041ab0bbe3fdeb507c6202a4a2
Elros <robotbrainify@gmail.com>
2014-04-18 12:48:57 -0400

    TTS Work

--- intellitalk/src/main/java/org/npenn/gifted/aacpecsapp/MainActivity.java
+++ intellitalk/src/main/java/org/npenn/gifted/aacpecsapp/MainActivity.java
@@ -3,16 +3,44 @@ package org.npenn.gifted.aacpecsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends BaseActivity {
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import org.lucasr.twowayview.TwoWayView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends BaseActivity implements TextToSpeech.OnInitListener {
+
+    TextToSpeech textToSpeech;
+    ImageButton playButton;
+    QueueAdapter queueAdapter;
+    List<Word> queue = Lists.newArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        queueAdapter = new QueueAdapter(this);
+        ((TwoWayView) findViewById(R.id.wordQueue)).setAdapter(queueAdapter);
+        playButton = (ImageButton) findViewById(R.id.playButton);
+        playButton.setEnabled(false);
+        textToSpeech = new TextToSpeech(this, this);
     }
 
     @Override
@@ -34,5 +62,105 @@ public class MainActivity extends BaseActivity {
 
     public void launchSettings(MenuItem item) {
         startActivity(new Intent(this, SettingsActivity.class));
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+            textToSpeech.setLanguage(Locale.US);
+            playButton.setEnabled(true);
+            textToSpeech.setOnUtteranceProgressListener(new UtteranceListener());
+            Toast.makeText(this, "Text to speech initialized properly! I can talk!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Test to speech initialization error! Try restarting the app.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void play(View view) {
+        playButton.setEnabled(false);
+
+        //Make text now
+        StringBuilder builder = new StringBuilder();
+        for (Word w : queue) {
+            if (builder.length() != 0 && !Lists.newArrayList(".", ",", "!", "?", ";", ":").contains(w.spokenText)) {
+                builder.append(' ');
+            }
+            builder.append(w.spokenText);
+        }
+        HashMap<String, String> params = Maps.newHashMap();
+        params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "Intellitalk speech");
+        textToSpeech.speak(builder.toString(), TextToSpeech.QUEUE_FLUSH, params);
+    }
+
+    @Override
+    protected void onDestroy() {
+        textToSpeech.shutdown();
+        super.onDestroy();
+    }
+
+    public static class QueueAdapter extends BaseAdapter {
+        MainActivity activity;
+
+        public QueueAdapter(MainActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public int getCount() {
+            return activity.queue.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = activity.getLayoutInflater().inflate(R.layout.queue_item, null);
+            }
+            Word w = activity.queue.get(i);
+            if (w.image != null) {
+                ((ImageView) view.findViewById(R.id.item_image)).setImageBitmap(w.image);
+            }
+            ((TextView) view.findViewById(R.id.item_text)).setText(w.displayText);
+            return view;
+        }
+    }
+
+    class UtteranceListener extends UtteranceProgressListener {
+
+        @Override
+        public void onStart(String s) {
+
+        }
+
+        @Override
+        public void onDone(String s) {
+            playButton.post(new Runnable() {
+                @Override
+                public void run() {
+                    playButton.setEnabled(true);
+                    //queue.clear();
+                }
+            });
+        }
+
+        @Override
+        public void onError(String s) {
+            playButton.post(new Runnable() {
+                @Override
+                public void run() {
+                    playButton.setEnabled(true);
+                    Toast.makeText(MainActivity.this, "Text to speech error! Please try again.", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
---
+++ intellitalk/src/main/java/org/npenn/gifted/aacpecsapp/Word.java
@@ -0,0 +1,15 @@
+package org.npenn.gifted.aacpecsapp;
+
+import android.graphics.Bitmap;
+
+public class Word {
+    public final String displayText;
+    public final String spokenText;
+    public final Bitmap image;
+
+    public Word(String displayText, String spokenText, Bitmap image) {
+        this.displayText = displayText;
+        this.spokenText = spokenText;
+        this.image = image;
+    }
+}
