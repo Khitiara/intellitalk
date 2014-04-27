5f77f048e0674abab76fea334ed7bae859d36235
Elros <robotbrainify@gmail.com>
2014-04-27 08:51:44 -0400

    Finish loading code; now the displaying and use.

--- intellitalk/src/main/java/org/npenn/gifted/aacpecsapp/ContentLoader.java
+++ intellitalk/src/main/java/org/npenn/gifted/aacpecsapp/ContentLoader.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Queues;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -15,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Queue;
 
 public class ContentLoader {
     public IntellitalkContent content;
@@ -47,33 +49,50 @@ public class ContentLoader {
         return BitmapFactory.decodeByteArray(imageRaw, 0, imageRaw.length, options);
     }
 
-    private void parseCategoriesEntry(JsonArray categories) {
-        for (JsonElement categoryRaw : categories) {
-            JsonObject categoryObject = categoryRaw.getAsJsonObject();
-            String name = categoryObject.get("name").getAsString();
-            Bitmap image = getBitmap(categoryObject);
-            JsonArray words = categoryObject.getAsJsonArray("content");
-            List<Word> content = Lists.newArrayList();
-            for (JsonElement wordRaw : words) {
-                JsonObject wordObject = wordRaw.getAsJsonObject();
-                String displayText = wordObject.get("display_text").getAsString();
-                String spokenText = wordObject.get("spoken_text").getAsString();
-                Bitmap wordImage = getBitmap(wordObject);
-                Word word = new Word(displayText, spokenText, wordImage);
-                content.add(word);
+    private void parseCategoriesEntry(JsonArray rootCategories) {
+        Queue<CategoryParseHelper> helpers = Queues.newSynchronousQueue();
+        helpers.add(new CategoryParseHelper(null, rootCategories));
+        while (!helpers.isEmpty()) {
+            CategoryParseHelper helper = helpers.remove();
+            for (JsonElement categoryRaw : helper.content) {
+                JsonObject categoryObject = categoryRaw.getAsJsonObject();
+                String name = categoryObject.get("name").getAsString();
+                Bitmap image = getBitmap(categoryObject);
+                JsonArray words = categoryObject.getAsJsonArray("content");
+                List<Word> content = Lists.newArrayList();
+                for (JsonElement wordRaw : words) {
+                    JsonObject wordObject = wordRaw.getAsJsonObject();
+                    String displayText = wordObject.get("display_text").getAsString();
+                    String spokenText = wordObject.get("spoken_text").getAsString();
+                    Bitmap wordImage = getBitmap(wordObject);
+                    Word word = new Word(displayText, spokenText, wordImage);
+                    content.add(word);
+                }
+                WordCategory category = new WordCategory(name, image, content);
+                if (helper.root == null) {
+                    this.content.categories.add(category);
+                } else {
+                    helper.root.nestedCategories.add(category);
+                }
+                if (categoryObject.has("categories")) {
+                    helpers.add(new CategoryParseHelper(category, categoryObject.getAsJsonArray("categories")));
+                }
             }
-            WordCategory category = new WordCategory(name, image, content);
-            this.content.categories.add(category);
-            parseCategoriesRecurse(categoryObject.getAsJsonArray("categories"), category);
         }
-    }
-
-    private void parseCategoriesRecurse(JsonArray nested, WordCategory category) {
-        //do like parseCategoriesEntry
     }
 
     public class IntellitalkContent {
         public List<Word> commonWords = Lists.newArrayList();
         public List<WordCategory> categories = Lists.newArrayList();
+    }
+
+    private class CategoryParseHelper {
+        WordCategory root;
+        JsonArray content;
+
+        private CategoryParseHelper(WordCategory root, JsonArray content) {
+            this.root = root;
+            this.content = content;
+        }
     }
 }
